6e324738ed627a4764964f4a587fb8721f9cebfa
Omar Khalil <omar_b_khalil@hotmail.com>
2025-12-11 01:28:30 +0100

    'dependency'

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Firebase plugin for all subprojects
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 allprojects {
